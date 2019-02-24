5b02c1f8119ce2605c6f8c11e3719d416de629ec
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2019-02-24 18:00:05 +0000

    initial commit

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        transform.position = target.transform.position;
+    }
+}
