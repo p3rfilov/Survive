c94fdcfdd01f68f54a2cc7f5aedc48374aa4ee4b
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2019-02-26 21:54:46 +0000

    implemented mouse-look

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,13 +21,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        isGrounded = Physics.CheckSphere(transform.position, groundDistance, ground, QueryTriggerInteraction.Ignore);
-
-        inputs = Vector3.zero;
-        inputs.x = Input.GetAxis("Horizontal");
-        inputs.z = Input.GetAxis("Vertical");
-        if (inputs != Vector3.zero)
-            transform.forward = viewRotation * inputs;
+        UpdateInputs();
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             body.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
@@ -39,5 +33,29 @@ public class PlayerMovement : MonoBehaviour
     {
         body.MovePosition(body.position + viewRotation * inputs * speed * Time.fixedDeltaTime);
         body.angularVelocity = Vector3.zero;
+    }
+
+    void UpdateInputs(bool mouseLook = true)
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundDistance, ground, QueryTriggerInteraction.Ignore);
+        inputs = Vector3.zero;
+        inputs.x = Input.GetAxis("Horizontal");
+        inputs.z = Input.GetAxis("Vertical");
+        if (mouseLook)
+        {
+            Ray ray;
+            RaycastHit hit;
+
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+            {
+                Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                transform.LookAt(targetPosition);
+            }
+        }
+        else if (inputs != Vector3.zero)
+        {
+            transform.forward = viewRotation * inputs;
+        }
     }
 }
