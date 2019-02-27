71d6dc3400196877158c0ba35f21dd079b8b0ef2
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2019-02-27 22:57:22 +0000

    implemented weapons and damage

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 10f;
+    public bool isAlive = true;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (health <= 0f)
+        {
+            Kill();
+        }
+    }
+
+    void Kill()
+    {
+        isAlive = false;
+        transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public bool mouseLook = true;
+    public float speed = 5f;
+    public float jumpStrength = 5f;
+    public float groundDistance = 0.2f;
+    public LayerMask ground;
+
+    private Rigidbody body;
+    private Vector3 inputs = Vector3.zero;
+    private bool isGrounded = true;
+    private Quaternion viewRotation = Quaternion.AngleAxis(45, Vector3.up);
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundDistance, ground, QueryTriggerInteraction.Ignore);
+
+        inputs = Vector3.zero;
+        inputs.x = Input.GetAxis("Horizontal");
+        inputs.z = Input.GetAxis("Vertical");
+        body.MovePosition(body.position + viewRotation * inputs * speed * Time.fixedDeltaTime);
+        body.angularVelocity = Vector3.zero;
+
+        if (mouseLook)
+        {
+            Ray ray;
+            RaycastHit hit;
+
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.CompareTag("Ground"))
+                {
+                    Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                    transform.LookAt(targetPosition);
+                }
+                else if (hit.transform.CompareTag("Enemy"))
+                {
+                    Vector3 targetPosition = new Vector3(hit.transform.position.x, transform.position.y, hit.transform.position.z);
+                    transform.LookAt(targetPosition);
+                }
+            }
+        }
+        else if (inputs != Vector3.zero)
+        {
+            transform.forward = viewRotation * inputs;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            body.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
+            isGrounded = false;
+        }
+    }
+}
