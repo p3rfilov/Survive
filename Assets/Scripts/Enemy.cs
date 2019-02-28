107fe85b983c260495047014b6f4269b509a0bba
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2019-02-28 22:53:36 +0000

    exploding zombies!

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,7 @@ public class Enemy : MonoBehaviour
 {
     public float health = 10f;
     public bool isAlive = true;
+    public float explosionForce = 3.5f;
 
     void Start()
     {
@@ -14,13 +15,35 @@ public class Enemy : MonoBehaviour
     {
         if (health <= 0f)
         {
-            Kill();
+            if (isAlive)
+            {
+                Kill();
+            }
         }
     }
 
     void Kill()
     {
+        Transform[] allParts;
+        Rigidbody body;
         isAlive = false;
-        transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+
+        Destroy(transform.GetComponent<Rigidbody>());
+        Destroy(transform.GetComponent<CapsuleCollider>());
+
+        allParts = transform.GetComponentsInChildren<Transform>();
+        foreach (var item in allParts)
+        {
+            if (item.parent)
+            {
+                Vector3 dir = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+
+                item.gameObject.AddComponent<BoxCollider>();
+                item.gameObject.AddComponent<Rigidbody>();
+                body = item.GetComponent<Rigidbody>();
+                body.AddForce(dir * explosionForce, ForceMode.Impulse);
+                body.angularVelocity = dir;
+            }
+        }
     }
 }
