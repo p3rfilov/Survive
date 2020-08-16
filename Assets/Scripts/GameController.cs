35015242d9535e3ffc0d1ab3e368d433d5e33652
Andrej Perfilov [home] <33530333+p3rfilov@users.noreply.github.com>
2020-08-16 18:20:33 +0100

    spawn zones

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public SpawnZone[] spawnZones;
+    public float spawnRate = 1f;
+
+    private float nextSpawn;
+
+
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            for (int i = 0; i < spawnZones.Length; i++)   
+            {
+                spawnZones[i].Spawn();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnZone.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SpawnZone : MonoBehaviour
+{
+    public GameObject[] objects;
+    [Range(0f, 100f)] public float spawnChance;
+
+    public void Spawn ()
+    {
+        int length = objects.Length;
+        if (length > 0)
+        {
+            if (spawnChance >= Random.Range(0f, 100f))
+            {
+                Instantiate(objects[Random.Range(0, length)], GetRandomPoint(), transform.rotation);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.3f);
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+
+    private Vector3 GetRandomPoint ()
+    {
+        return transform.position + new Vector3(
+           (Random.value - 0.5f) * transform.localScale.x,
+           (Random.value - 0.5f) * transform.localScale.y,
+           (Random.value - 0.5f) * transform.localScale.z
+        );
+    }
+}
